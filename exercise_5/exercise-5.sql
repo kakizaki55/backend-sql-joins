49cec3824032992b6f320133776258ad7c040639
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-03-21 12:32:32 -0700

    done wtih number 6

--- exercise_5/exercise-5.sql
+++ exercise_5/exercise-5.sql
@@ -1,2 +1,10 @@
 -- find all film titles that an actor with the last_name Davis appears in
 -- Similar to exercise 3 but with actor, film_actor, film
+SELECT
+    title
+FROM
+    actor
+    INNER JOIN film_actor ON actor.actor_id = film_actor.actor_id
+    INNER JOIN film ON film.film_id = film_actor.film_id
+WHERE
+    actor.last_name = 'Davis'
